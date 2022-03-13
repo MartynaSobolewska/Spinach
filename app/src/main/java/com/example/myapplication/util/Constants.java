915715c3ca580b9774ccddcbabf7538c99c14671
MartynaSobolewskaExe <ms1143@exeter.ac.uk>
2022-03-13 16:22:02 +0000

    constants and Icons

---
+++ app/src/main/java/com/example/myapplication/util/Constants.java
@@ -0,0 +1,6 @@
+package com.example.myapplication.util;
+
+public class Constants {
+    public static final String BASE_URL = "https://tasty.p.rapidapi.com/";
+    public static final String API_KEY = "51cb525385msh484bcbd78e109b8p18aac8jsn35f2f0bbe8db";
+}
